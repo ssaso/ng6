ea384637bfd965f453ee32b50c197317eaaf9737
ssaso <ssaso@live.com>
2018-06-08 09:57:07 +0200

    Auto-generated commit

--- ars-components/lib/checkbox/checkbox.component.d.ts
+++
@@ -1,5 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class CheckboxComponent implements OnInit {
-    constructor();
-    ngOnInit(): void;
-}
--- ars-components/public_api.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/ars-components.service';
-export * from './lib/ars-components.component';
-export * from './lib/ars-components.module';
